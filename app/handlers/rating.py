e73e0294e5e5a4102b4e521d74a44974067527d9
KFeyn <kir-fein@yandex.ru>
2021-07-25 14:14:07 +0300

    Добавлен файл с функции отправки изображения с рейтингом

---
+++ app/handlers/rating.py
@@ -0,0 +1,31 @@
+from aiogram import Dispatcher, types
+from .. import dbworker
+import seaborn as sns
+import os
+import matplotlib.pyplot as plt
+sns.set()
+sns.set(rc={'figure.figsize': (15, 7)})
+
+
+async def see_rating(message: types.Message):
+    """
+    Функция отправки изображения с рейтингом
+
+    :param message: сообщение
+    """
+    top5 = dbworker.dbase.execute_query('select fio, rating from names.default_names order by rating desc limit 5'
+                                        , with_result=True)
+    sns.barplot(x='rating', y='fio', data=top5,  palette="Blues_d")
+    file_name = f'test_{message.from_user.id}.png'
+    plt.savefig(file_name)
+    await message.answer_photo(photo=open(file_name, 'rb'))
+    os.remove(file_name)
+
+
+def register_handlers_rating(dp: Dispatcher):
+    """
+    Регистрация графических сообщений в диспетчере
+
+    :param dp: диспетчер
+    """
+    dp.register_message_handler(see_rating, commands="rating")
